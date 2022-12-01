85db9b80674abe41d0e93f828e3a55187cb81ea2
ZhenbangYou <82886739+ZhenbangYou@users.noreply.github.com>
2022-11-30 16:07:42 -0800

    Update segment-tree.fsx

--- segment-tree.fsx
+++ segment-tree.fsx
@@ -1,21 +1,21 @@
 module SegmentTree =
-    type treeNode =
-        | Leaf of leafType
-        | Internal of internalType
+    type treeNode<'a> =
+        | Leaf of leafType<'a>
+        | Internal of internalType<'a>
 
-    and internalType =
-        { leftChild: treeNode
-          rightChild: treeNode
+    and internalType<'a> =
+        { leftChild: treeNode<'a>
+          rightChild: treeNode<'a>
           low: int
           high: int
-          sum: int
-          addend: int }
+          sum: 'a
+          addend: 'a }
 
-    and leafType = { value: int }
+    and leafType<'a> = { value: 'a }
 
-    type t = treeNode
+    type t<'a> = treeNode<'a>
 
-    let create (numbers: int []) =
+    let create (numbers: 'a []) (zeroValue: 'a) (add: 'a -> 'a -> 'a) =
         let rec create' low high =
             if low = high then
                 (Leaf { value = numbers[low] }, numbers[low])
@@ -29,17 +29,17 @@ module SegmentTree =
                       rightChild = rightChild
                       low = low
                       high = high
-                      sum = lsum + rsum
-                      addend = 0 },
-                 lsum + rsum)
+                      sum = add lsum rsum
+                      addend = zeroValue },
+                 add lsum rsum)
 
         let res, _ = create' 0 (numbers.Length - 1)
         res
 
-    let queryRange (node: t) (low: int) (high: int) : int =
-        let rec queryRange' (node: treeNode) (low: int) (high: int) (accAddend: int) : int =
+    let queryRange (node: t<'a>) (low: int) (high: int) (zeroValue: 'a) (add: 'a -> 'a -> 'a) : 'a =
+        let rec queryRange' (node: treeNode<'a>) (low: int) (high: int) (accAddend: 'a) : 'a =
             match node with
-            | Leaf { value = value } -> accAddend + value
+            | Leaf { value = value } -> add accAddend value
             | Internal { leftChild = leftChild
                          rightChild = rightChild
                          low = l
@@ -49,20 +49,21 @@ module SegmentTree =
                 let mid = (l + h) / 2
 
                 if low = l && high = h then
-                    sum + ((high - low + 1) * accAddend)
+                    add sum (List.reduce add [ for _ in low..high -> accAddend ])
                 else if high <= mid then
-                    queryRange' leftChild low high (accAddend + addend)
+                    queryRange' leftChild low high (add accAddend addend)
                 else if low > mid then
-                    queryRange' rightChild low high (accAddend + addend)
+                    queryRange' rightChild low high (add accAddend addend)
                 else
-                    queryRange' leftChild low mid (accAddend + addend)
-                    + queryRange' rightChild (mid + 1) high (accAddend + addend)
+                    add
+                        (queryRange' leftChild low mid (add accAddend addend))
+                        (queryRange' rightChild (mid + 1) high (add accAddend addend))
 
-        queryRange' node low high 0
+        queryRange' node low high zeroValue
 
-    let rec updateRange (node: t) (low: int) (high: int) (delta: int) =
+    let rec updateRange (node: t<'a>) (low: int) (high: int) (delta: 'a) (zeroValue: 'a) (add: 'a -> 'a -> 'a) =
         match node with
-        | Leaf { value = value } -> Leaf { value = value + delta }
+        | Leaf { value = value } -> Leaf { value = add value delta }
         | Internal { leftChild = leftChild
                      rightChild = rightChild
                      low = l
@@ -77,29 +78,29 @@ module SegmentTree =
                       rightChild = rightChild
                       low = l
                       high = h
-                      sum = sum + ((high - low + 1) * delta)
-                      addend = addend + delta }
+                      sum = add sum (List.reduce add [ for _ in low..high -> delta ])
+                      addend = add addend delta }
             else if high <= mid then
                 Internal
-                    { leftChild = updateRange leftChild low high delta
+                    { leftChild = updateRange leftChild low high delta zeroValue add
                       rightChild = rightChild
                       low = l
                       high = h
-                      sum = sum + ((high - low + 1) * delta)
+                      sum = add sum (List.reduce add [ for _ in low..high -> delta ])
                       addend = addend }
             else if low > mid then
                 Internal
                     { leftChild = leftChild
-                      rightChild = updateRange rightChild low high delta
+                      rightChild = updateRange rightChild low high delta zeroValue add
                       low = l
                       high = h
-                      sum = sum + ((high - low + 1) * delta)
+                      sum = add sum (List.reduce add [ for _ in low..high -> delta ])
                       addend = addend }
             else
                 Internal
-                    { leftChild = updateRange leftChild low mid delta
-                      rightChild = updateRange rightChild (mid + 1) high delta
+                    { leftChild = updateRange leftChild low mid delta zeroValue add
+                      rightChild = updateRange rightChild (mid + 1) high delta zeroValue add
                       low = l
                       high = h
-                      sum = sum + ((high - low + 1) * delta)
+                      sum = add sum (List.reduce add [ for _ in low..high -> delta ])
                       addend = addend }
